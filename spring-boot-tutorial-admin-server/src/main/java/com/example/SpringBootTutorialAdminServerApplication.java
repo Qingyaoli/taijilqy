8e38ff4911d59b45bbd1f24e02c7a2ef550302fd
Qingyaoli <17862107647@163.com>
2018-11-26 09:45:45 +0800

    spring-boot-admin-server更新

--- spring-boot-tutorial-admin-server/src/main/java/com/example/SpringBootTutorialAdminServerApplication.java
+++ spring-boot-tutorial-admin-server/src/main/java/com/example/SpringBootTutorialAdminServerApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
